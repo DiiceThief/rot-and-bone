0400963540c6f45fcf3c110bba6ef727d4b8da25
DiiceThief <solarhog87@gmail.com>
2026-04-30 09:26:35 -0600

    first commit

---
+++ diices-dastardly-dead-26.1.2/src/client/java/net/tinydiicethief/diicesdastardlydead/client/DiicesDastardlyDeadClient.java
@@ -0,0 +1,14 @@
+package net.tinydiicethief.diicesdastardlydead.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DiicesDastardlyDeadClient implements ClientModInitializer {
+	public static final String MOD_ID = "diices_dastardly_dead";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+	}
+}
